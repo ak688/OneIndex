7899fcc47e954ae03a84f32a167b4b12c9ed26c2
ak688 <56494634+ak688@users.noreply.github.com>
2020-10-23 11:33:02 +0800

    Update AdminController.php

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -271,7 +271,7 @@ class AdminController
         else
         {
             // 非https,调用ju.tn中转
-            $redirect_uri = 'https://oneindex.github.io/';
+            $redirect_uri = 'https://akyun.herokuapp.com/';
         }
 
         $ru = "https://developer.microsoft.com/en-us/graph/quick-start?appID=_appId_&appName=_appName_&redirectUrl={$redirect_uri}&platform=option-php";
